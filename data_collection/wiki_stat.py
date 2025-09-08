09e6dc6d055f5f44c1ab7112db5951b0bd8704d2
davidhhhhhhh <57957067+davidhhhhhhh@users.noreply.github.com>
2025-09-08 17:41:52 -0400

    adding wiki stats macro

--- data_collection/wiki_stat.py
+++ data_collection/wiki_stat.py
@@ -1,4 +1,5 @@
 from responses_collection.data_processor import get_wiki_content
+from front_end_pipeline.src.data import _filter_not_relevant_wiki
 
 wiki_df = get_wiki_content()
 print(wiki_df.columns)
@@ -12,4 +13,12 @@ below_5th = wiki_df[wiki_df['token_length'] < token_length_5th]
 print(f"Number of entries below 5th percentile: {below_5th.shape[0]}")
 
 # export below 5th percentile for manual examination 
-below_5th.to_csv("data_collection/below_5th_percentile.csv", index=False)
+# below_5th.to_csv("data_collection/below_5th_percentile.csv", index=False)
+
+filtered_wiki_df = _filter_not_relevant_wiki(wiki_df)
+print(f"Number of entries after filtering: {filtered_wiki_df.shape[0]}")
+print(filtered_wiki_df.columns)
+print(filtered_wiki_df['content_id'].nunique())
+print(filtered_wiki_df['subcategory'].nunique())
+print(filtered_wiki_df['category'].nunique())
+
